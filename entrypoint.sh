2e552dbc5b5880cd3cb3cc37f6784ce7b4583345
Greyari <159263690+Greyari@users.noreply.github.com>
2025-08-05 15:47:33 +0700

    keperluan deploy 5

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ until php artisan migrate:status > /dev/null 2>&1; do
   sleep 2
 done
 
+# Cache config agar ENV dari Railway dibaca
+php artisan config:clear
+php artisan config:cache
+
 # Jalankan migrate dan seed
 echo "Menjalankan migrate dan seed..."
 php artisan migrate --force
