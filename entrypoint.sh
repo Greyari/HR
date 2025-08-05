bd14171f8595c86ad0583529eb31c993cc48f6e5
Greyari <159263690+Greyari@users.noreply.github.com>
2025-08-05 15:49:59 +0700

    keperluan deploy 6

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Tunggu MySQL agar siap
+# Tunggu DB siap
 echo "Menunggu database siap..."
 until php artisan migrate:status > /dev/null 2>&1; do
   sleep 2
 done
 
-# Cache config agar ENV dari Railway dibaca
+# Clear dan cache config agar baca dari Railway ENV
 php artisan config:clear
 php artisan config:cache
 
-# Jalankan migrate dan seed
+# Jalankan migrate dan seeder
 echo "Menjalankan migrate dan seed..."
 php artisan migrate --force
 php artisan db:seed --force
 
-# Jalankan Laravel server
+# Jalankan Laravel
 php artisan serve --host=0.0.0.0 --port=8080
