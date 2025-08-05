c0f786ac5e392e57273ab60c3905b40d10f452c2
Greyari <159263690+Greyari@users.noreply.github.com>
2025-08-05 16:43:59 +0700

    keperluan deploy 10

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,14 @@ Route::post('/admin/departemen', [DepartemenController::class, 'store'])->name('
 Route::delete('/departemen/{id}', [DepartemenController::class, 'destroy'])->name('departemen.destroy');
 Route::put('/departemen/{id}', [DepartemenController::class, 'update'])->name('departemen.update');
 Route::get('/admin/departemen/search', [DepartemenController::class, 'search'])->name('departemen.search');
+
+
+Route::get('/debug-db', function () {
+    return [
+        'host' => env('DB_HOST'),
+        'port' => env('DB_PORT'),
+        'username' => env('DB_USERNAME'),
+        'password' => env('DB_PASSWORD'),
+        'database' => env('DB_DATABASE'),
+    ];
+});
