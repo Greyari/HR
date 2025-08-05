7f5f9766b1985f5d875a2172ad14c372d405205f
Greyari <159263690+Greyari@users.noreply.github.com>
2025-08-05 21:53:04 +0700

    keperluan deploy 14

--- resources/views/layouts/mainAdmin.blade.php
+++ resources/views/layouts/mainAdmin.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title }}</title>
-    @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 
 <body class="min-h-screen flex flex-col bg-gray-50">
