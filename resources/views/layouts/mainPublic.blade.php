83cdc6a9ccc1f7ad5919c791ea94bc280bba252a
Greyari <159263690+Greyari@users.noreply.github.com>
2025-08-06 10:38:01 +0700

    keperluan deploy 30

--- resources/views/layouts/mainPublic.blade.php
+++ resources/views/layouts/mainPublic.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-MmjC4X14.css') }}">
-    <script type="module" src="{{ secure_asset('build/assets/app-DFlgualb.js') }}"></script>
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-DBQOvqF2.css') }}">
+    <script type="module" src="{{ secure_asset('build/assets/app-B1unrRu7.js') }}"></script>
 </head>
 <body class="min-h-screen bg-white">
 
