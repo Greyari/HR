f3bada45c7965be7b72a1849f27cfe7f7c8681a8
Greyari <159263690+Greyari@users.noreply.github.com>
2025-08-10 17:52:18 +0700

    minggu untuk deploy 8-10-2025

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], // Ganti '*' ke asal Flutter Web misal: ['http://localhost:57718']
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
