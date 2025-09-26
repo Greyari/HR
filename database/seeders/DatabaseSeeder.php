75ec152dd935f2ba639f9e780d441949f3352e9e
Greyari <159263690+Greyari@users.noreply.github.com>
2025-09-26 16:02:06 +0700

    set up seeder 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(FiturSeeder::class);
         $this->call(HakAksesSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(KantorSeeder::class);
     }
 }
