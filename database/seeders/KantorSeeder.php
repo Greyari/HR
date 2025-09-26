74e74d3ed6b83b0bd27acd004b74441d8165fcd5
Greyari <159263690+Greyari@users.noreply.github.com>
2025-09-26 16:02:06 +0700

    set up seeder 2

---
+++ database/seeders/KantorSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class KantorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('kantor')->insert([
+            'jam_masuk'             => '08:00:00',
+            'jam_keluar'            => '17:00:00',
+            'minimal_keterlambatan' => 10,
+            'lat'                   => 1.125027673628618,
+            'lng'                   => 104.029007793717,
+            'radius_meter'          => 100,
+            'jatah_cuti_tahunan'    => 12,
+            'created_at'            => now(),
+            'updated_at'            => now(),
+        ]);
+    }
+}
