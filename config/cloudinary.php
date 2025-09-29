1436d48fe2f5c046c544bffea72e03e46980021a
Greyari <159263690+Greyari@users.noreply.github.com>
2025-09-29 15:40:20 +0700

    memperbarui observer dan melakukan update untuk upload vidio absensi dan lampiran tugas deploy 24

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -5,9 +5,9 @@ return [
     'url' => env('CLOUDINARY_URL'),
 
     'cloud' => [
-        'cloud_name' => env('CLOUDINARY_CLOUD_NAME') ? trim(env('CLOUDINARY_CLOUD_NAME'), '"') : null,
-        'api_key'    => env('CLOUDINARY_API_KEY') ? trim(env('CLOUDINARY_API_KEY'), '"') : null,
-        'api_secret' => env('CLOUDINARY_API_SECRET') ? trim(env('CLOUDINARY_API_SECRET'), '"') : null,
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
     ],
 
     'url_options' => [
