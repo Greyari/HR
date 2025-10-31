ae6ba37c162de0f57d140e9da4348ba2fdebae1c
Greyari <159263690+Greyari@users.noreply.github.com>
2025-10-31 15:48:57 +0700

    update untuk fix bug cors put delete 2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
-        \App\Http\Middleware\CorsMiddleware::class, 
+        \App\Http\Middleware\CorsMiddleware::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -27,7 +27,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Fruitcake\Cors\HandleCors::class,
+            \App\Http\Middleware\CorsMiddleware::class, // ✅ custom cors kamu aktif di semua API
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
